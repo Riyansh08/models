09176cf0df1935283285a968512d653843650592
Riyansh Shah <theriyanshshah@icloud.com>
2025-04-08 16:00:00 -0700

    LLAMA-2

--- languageModels/LLAMA/Llama-1-3/llama-2.py
+++ languageModels/LLAMA/Llama-1-3/llama-2.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 import math 
 from inspect import isfunction
 """
-NOTE :#The Llama 1 architecture is similar to Llama 2, except that Llama 2 has a larger context window.
+NOTE : The Llama 1 architecture is almost similar to Llama 2, except that Llama 2 has a larger context window and other minor changes .
 Llama 2 has a context window of 4096 tokens, while Llama 1 has a context window of 2048 tokens.
 """
 @dataclass 
@@ -21,15 +21,54 @@ class ModelConfig:
     norm_eps : float = 1e-5
     device : str = field(default_factory=lambda : 'cuda' if torch.cuda.is_available() else 'cpu')
 
-
     
 #Llamma 2 uses RMSNorm instead of LayerNorm.
 #For more details, please see the paper Root Mean Square Layer Normalization (2019)
-class RMSNorn(nn.Module):
+class RMSNorm(nn.Module):
     def __init__(self , emb_dim , eps = 1e-6):
-        pass
+        super(RMSNorm , self).__init__()
+        self.eps = eps
+        self.dim = emb_dim
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        
+    def forward(self , x ):
+        means = x.pow(2).mean(dim = -1 , keepdim = True)
+        x_normalized = x * torch.rqrt(means + self.eps)
+        x_normalized = x_normalized * self.scale
+#Activation Function 
+#Llama 2 uses the SiLU activation function instead of GELU.
+class SiLU(nn.Module):
+    def __init__(self):
+        super(SiLU , self).__init__()
+    def forward(self , x ):
+        return x * 1/(1 +torch.exp(-x))
+    
 class ROPE(nn.Module):
-    pass
-       
+    def __init__(self , h_dim , seq_len):
+        super(ROPE , self).__init__()
+        self.h_dim = h_dim
+        self.seq_len = seq_len 
+        assert self.h_dim % 2 == 0 , "head dimensions must be even "
+        theta_numerator = torch.arrange(0 , self.h_dim ,2 , dtype = torch.float32)
+        theta = 1.0 / torch.pow(10000 , theta_numerator / self.h_dim)
+        m = torch.arange(self.seq_len , dtype = torch.float32).unsqueeze(1)       
+        freqs = torch.matmul(m , theta.unsqueeze(0))
+        
+        
+        self.freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
+        self.register_buffer("freqs_complex", self.freqs_complex)
+          
+    def forward(self, x, start_pos) :
+        batch_size, seq_len, dim = x.shape
+        assert dim == self.dim, "dim must be equal to self.dim"
+        x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+        freq_complex = self.freqs_complex[start_pos:start_pos + seq_len]
+        freq_complex = freq_complex.unsqueeze(0).unsqueeze(2)
+        x_rotated = x_complex * freq_complex
+        x_out = torch.view_as_real(x_rotated)
+        x_out = x_out.reshape(*x.shape)
+        return x_out.type_as(x).to(x.device)    
+class MultiHeadAttention(nn.Module):
+    pass      
 class Llama2(nn.Module):
     pass
