29a73fcd8c665cd1a544f31ca32090914b8d6415
Riyansh Shah <theriyanshshah@icloud.com>
2025-04-08 20:52:06 -0700

    Llama-3.2 tomorrow

---
+++ languageModels/LLAMA/Llama-1-3/llama-3.py
@@ -0,0 +1,8 @@
+#Llama-3 8B implementation 
+import math
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Llama3(nn.Module):
+    pass
---
+++ languageModels/LLAMA/Llama-1-3/train_Llama2.py
@@ -0,0 +1,1 @@
+#TRAINING CODE 
