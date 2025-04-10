240006ba6593a77509578acafa9911bd7014c632
Riyansh Shah <theriyanshshah@icloud.com>
2025-04-10 01:53:01 -0700

    VAE 1/2 using simple MLP

--- unsupervised/VAE/vae.py
+++ unsupervised/VAE/vae.py
@@ -1,4 +1,5 @@
-#VAE - using U-Net architecture
+#VAE - using simple MLP 
+#NOTE - dataset loading and training loop not imcluded
 import torch
 import torch.nn as nn 
 import torch.nn.functional as F
@@ -45,7 +46,33 @@ class Decoder(nn.Module):
     def __init__(self , config : Config):
         super(Decoder , self).__init__()
         self.config = config 
-        
-              
+        self.fc1 = nn.Linear(config.z_dim , config.hidden_dim2)
+        self.fc2 = nn.Linear(config.hidden_dim2 , config.hidden_dim1)
+        self.outputfc = nn.Linear(config.hidden_dim1 , config.x_dim)
+        self.LeakyReLU = nn.LeakyReLU(0.2)
+    def forward(self, x ):
+        x = self.LeakyReLU(self.fc1(x))
+        x = self.LeakyReLU(self.fc2(x))
+        x = self.outputfc(x)
+        return x             
 class VAE(nn.Module):
-    pass
+    def __init__(self , config : Config):
+        super(VAE , self).__init__()
+        self.config = config
+        self.encoder = ENCODER(config)
+        self.decoder = Decoder(config)
+    def reparameterize(self , mean  , logvar):
+        e = torch.randn_like(logvar)
+        std = torch.exp(logvar * 0.5)
+        return mean + std * e
+        
+    def forward(self ,x ):
+        mean , logvar = self.encoder
+        e = self.reparameterize(mean , logvar)
+        x_out = self.decoder(e)
+        return x_out , mean , logvar       
+#Loss Function
+def loss_function(recon_x , x , mean , logvar):
+    BCE = F.binary_cross_entropy(recon_x , x , reduction = 'sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
+    return BCE + KLD
--- unsupervised/VAE/vae_unet.py
+++ unsupervised/VAE/vae_unet.py
@@ -9,6 +9,28 @@ from typing import List, Optional, Tuple
 # NOTE - output size - [input - Kernel + 2*padding] / stride + 1
 @dataclass
 class Config:
+    """
+    Configuration class for the VAE model.
+    """
+    batch_size : int = 64
+    num_epochs : int 
+    learning_rate : float = 0.01 
+    x_dim : int 
+    z_dim : int
+    hidden_dim1 : int
+    hidden_dim2 : int
+    hidden_dim3 : int
+    dropout : float = 0.2
+    in_channels : int
+    out_channelss : int
+    kernel_size :int
+    stride : int
+    padding : int 
+#CNN from Scratch 
+class Convolutional:
+    def __init__(self ):
+        pass
+class Encoder(nn.Module):
     pass
 class VAE(nn.Module):
     pass
