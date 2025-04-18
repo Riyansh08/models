9a35a2c2080adde1ed1bf2e778573c7bcbe049b5
Riyansh Shah <theriyanshshah@icloud.com>
2025-04-18 15:51:06 -0700

    LoRA done

--- finetune/scripts/lora.py
+++ finetune/scripts/lora.py
@@ -1,8 +1,15 @@
 #LORA - Small implemtation 
-
 import torch
 import torch.nn as nn
-
 class LoRA(nn.Module):
-    def __init__(self ):
-        pass 
+    def __init__(self , in_features , out_features , rank , alpha , en : bool = True):
+        super().__init__()
+        self.A = nn.Parameter(torch.zeros(rank, out_features))
+        self.B = nn.Parameter(torch.zeros(in_features , rank))
+        self.scale = alpha/ rank 
+        self.en = True
+    def forward(self , weights):
+        if self.en:
+            return weights + torch.matmul(self.B , self.A).view(weights.shape) * self.scale
+        else:
+            return weights
