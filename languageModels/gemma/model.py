1b14185746f6bb2038f96a8266e778c100fba0da
Riyansh Shah <theriyanshshah@icloud.com>
2025-04-19 04:33:51 -0700

    gemma 1/3

--- languageModels/gemma/model.py
+++ languageModels/gemma/model.py
@@ -26,16 +26,49 @@ class GemmaConfig:
     rope_theta : int
     attention_output : bool = True
     pad_token_id : int
+    rps_norm_eps : int   
 #RMS NORM - ROOT MEAN SQUARE NORMALIZATION 
 class RMSNorm(nn.Module):
     def __init__(self , config: GemmaConfig ):
         super().__init__()
         self.dim_size = config.dim_size
-        
-        
-    
-
+        self.eps = config.rps_norm_eps
+        self.scale = nn.Parameter(torch.ones(self.dim_size))
+        self.shift = nn.Parameter(torch.zeros(self.dim_size))
+    def forward(self , x):
+        normalize = torch.sqrt(x.pow(2).mean(-1 , keepdim =True) + self.eps)
+        return x / normalize
+# GATED MULTI-LAYER PERCEPTRON - GEMMA 
+class MLP(nn.Module):
+    def __init__(self, config : GemmaConfig):
+        self.gate_proj = nn.Linear(config.dim_size , config.hidden_size)
+        self.up_proj = nn.Linear(config.dim_size , config.hidden_size)
+        self.ff3 = nn.Linear(config.hidden_size , config.dim_size)
+        self.activation =nn.GELU()
+    def forward(self , x ):
+        gate = self.gate_proj(x)
+        up = self.up_proj(x)
+        activated  = self.activation(gate)
+        out = activated * up
+        output = self.ff3(out)
+        return output 
 #ROPE - ROTARY POSITIONAL EMBEDDING 
 class Rope:
     def __init__():
         pass
+# CLASS KV-CACHE DURING INFERENCE 
+class KVCache:
+    def __init__(self):
+        self.key_cache = []
+        self.value_cache = []
+    def num_items(self):
+        if len(self.key_cache)==0:
+            return 0 
+        else:
+            return self.key_cache[0].shape[-2]
+    def update(self , key ,values , layer_index):
+        if len(self.key_cache) <= layer_index:
+            self.key_cache.append(key)
+            self.value_cache.append(values)
+        else:
+            pass
