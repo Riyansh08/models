e431ba0824cabb74b6a634ab92cb8697bc290b81
Riyansh Shah <theriyanshshah@icloud.com>
2025-05-19 19:08:00 -0700

    model of NanoGPT - w Flash Attention done

--- languageModels/nanoGPT/model.py
+++ languageModels/nanoGPT/model.py
@@ -260,14 +260,71 @@ def from_pretrained(cls, model_type, config: Optional[NanoGPTConfig] = None, ove
 
         return model
     
-    def configure_optimizers(self):
-        pass 
-    
-        
-    
-    def estimate_flops(self):
-        pass
-
+    def configure_optimizers(self , weight_decay , learning_rate , betas):
+        param_dict = { pn: p for pn , p in self.named_parameters()}
+        param_dict = {pn: p for pn , p in param_dict.items() if p.requires_grad}
+        decay_params = {pn: p for pn , p in param_dict if p.dim() >=2}
+        no_decay_params = {pn: p for pn , p in param_dict if p.dim() <2}
+        optim_groups = [
+            {"params": decay_params.values() , "weight_decay": weight_decay},
+            {"params": no_decay_params.values() , "weight_decay": 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in no_decay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(no_decay_params)}, with {num_nodecay_params:,} parameters")
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available 
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+        print(f"using fused AdamW: {use_fused}")
+        return optimizer  
+    # Estimate TOTAL FLOPS - usually the chinchilla rule of thumb is 6ND - N is number of parameters and D is the number of data points (here tokens) 
+    #From Andrej Karpathy's NanoGPT
+    def estimate_mfu(self, fwdbwd_per_iter, dt):
+        """ estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS """
+        # first estimate the number of flops we do per iteration.
+        # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
+        N = self.get_num_params()
+        cfg = self.config
+        L, H, Q, T = cfg.n_layer, cfg.n_head, cfg.n_embd//cfg.n_head, cfg.block_size
+        flops_per_token = 6*N + 12*L*H*Q*T
+        flops_per_fwdbwd = flops_per_token * T
+        flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
+        # express our flops throughput as ratio of A100 bfloat16 peak flops
+        flops_achieved = flops_per_iter * (1.0/dt) # per second
+        flops_promised = 312e12 # A100 GPU bfloat16 peak flops is 312 TFLOPS
+        mfu = flops_achieved / flops_promised
+        return mfu
     @torch.no_grad()
-    def generate(self):
-        pass
+    def generate(self , idx ,max_new_tokens , temperature = 1.0 , top_k = None ):
+        
+        """_summary_
+
+        Args:
+            idx (_type_): _description_
+            max_new_tokens (_type_): _description_
+            temperature (float, optional): _description_. Defaults to 1.0.
+            top_k (_type_, optional): _description_. Defaults to None.
+        """ 
+        for _ in range(max_new_tokens):
+            # if the sequence context is growing too long we must crop it at block_size
+            idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+            # forward the model to get the logits for the index in the sequence
+            logits, _ = self(idx_cond)
+            # pluck the logits at the final step and scale by desired temperature
+            logits = logits[:, -1, :] / temperature
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float('Inf')
+            # apply softmax to convert logits to (normalized) probabilities
+            probs = F.softmax(logits, dim=-1)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)
+            # append sampled index to the running sequence and continue
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx 
+
+print("DONE SUCCESSFULLY!")
