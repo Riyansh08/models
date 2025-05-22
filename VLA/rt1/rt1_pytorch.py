d5baa2c04db66cc8067bf736fd8636660e46c34a
Riyansh Shah <theriyanshshah@icloud.com>
2025-05-21 19:40:41 -0700

    reinforce and ppo done

--- VLA/rt1/rt1_pytorch.py
+++ VLA/rt1/rt1_pytorch.py
@@ -17,8 +17,17 @@ from beartype import beartype
 from einops.layers.torch import Rearrange, Reduce
 from functools import partial
 from classifier_free_guidance_pytorch import TextConditioner, AttentionTextConditioner , classifier_free_guidance
-
+# Useless Helpers 
 def exists(val):
     return val is not None 
+def default(val , d):
+    return val if exists(val) else d 
+def cast_tuple(val , length = 1):
+    return val if isinstance(val , tuple) else ((val ,) * length)
+def pack_one(x, pattern):
+    return pack([x], pattern)
+def unpack_one(x, ps, pattern):
+    return unpack(x, ps, pattern)[0]
 
+#Sinusoidal Positional Embedding 
 #TO-DO 
